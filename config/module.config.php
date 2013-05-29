2cb36e4276540f9b86c12c8950d325ed002fe4d3
darkmatus <admin@adventure-hack.de>
2013-05-29 07:54:36 +0200

    Update module.config.php
    remove empty controller and router array

--- config/module.config.php
+++ config/module.config.php
@@ -6,12 +6,6 @@ return array(
             'template_path_stack' => array(),
         ),
 
-        'router' => include __DIR__ . '',
-
-        'controllers' => array(
-            'invokables' => array(
-            ),
-        ),
         'view_helpers' => array(
             'invokables'=> array(
                 'bbcode' => 'ZF2Bbcode\View\Helper\Bbcode'
